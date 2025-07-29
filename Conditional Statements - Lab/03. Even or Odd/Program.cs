f563be13bbf24d21d5ad344133676d78fcb263fc
Sabri Durmaz <drmz.sbr@gmail.com>
2025-07-29 22:19:40 +0300

    Conditional Statements - Lab

---
+++ Conditional Statements - Lab/03. Even or Odd/Program.cs
@@ -0,0 +1,9 @@
+﻿int isEven = int.Parse(Console.ReadLine());
+if (isEven % 2 == 0)
+{
+    Console.WriteLine("even");
+}
+else
+{
+    Console.WriteLine("odd");
+}
---
+++ Conditional Statements - Lab/07. Area of Figures/Program.cs
@@ -0,0 +1,25 @@
+﻿string geofig = Console.ReadLine();
+double S = 0;
+if (geofig == "square")
+{
+    double sideA = double.Parse(Console.ReadLine());
+    S = sideA * sideA;
+}
+else if (geofig == "rectangle")
+{
+    double sideA = double.Parse(Console.ReadLine());
+    double sideB = double.Parse(Console.ReadLine());
+    S = sideA * sideB;
+}
+else if (geofig == "circle")
+{
+    double radius = double.Parse(Console.ReadLine());
+    S = Math.PI * Math.Pow(radius, 2);
+}
+else if (geofig == "triangle")
+{
+    double sideA = double.Parse(Console.ReadLine());
+    double sideB = double.Parse(Console.ReadLine());
+    S = sideA * sideB / 2;
+}
+Console.WriteLine($"{S:F3}");
