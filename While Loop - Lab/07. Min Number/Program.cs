ebda66dda76ee63815f217c8d851df7a8c461a22
Sabri Durmaz <drmz.sbr@gmail.com>
2025-07-29 22:36:42 +0300

    While Loop - Lab

---
+++ While Loop - Lab/07. Min Number/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int minNumber = int.MaxValue; 
+
+        while (true)
+        {
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "stop")
+            {
+                break;
+            }
+
+            int number;
+            if (int.TryParse(input, out number))
+            {
+                if (number < minNumber)
+                {
+                    minNumber = number;
+                }
+            }
+        }
+
+        if (minNumber != int.MaxValue)
+        {
+            Console.WriteLine(minNumber);
+        }
+    }
+}
