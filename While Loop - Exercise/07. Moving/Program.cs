0f05c97e58da6cc000416eadac4d59758018aafd
Sabri Durmaz <drmz.sbr@gmail.com>
2025-07-29 22:42:50 +0300

    While Loop - Exercise

---
+++ While Loop - Exercise/07. Moving/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int width = int.Parse(Console.ReadLine());
+        int length = int.Parse(Console.ReadLine());
+        int height = int.Parse(Console.ReadLine());
+
+        int freeSpace = width * length * height;
+        int usedSpace = 0;
+
+        while (true)
+        {
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "done")
+            {
+                break;
+            }
+
+            int boxes = int.Parse(input);
+            usedSpace += boxes;
+
+            if (usedSpace > freeSpace)
+            {
+                Console.WriteLine($"No more free space! You need {usedSpace - freeSpace} Cubic meters more.");
+                return;
+            }
+        }
+
+        int remainingSpace = freeSpace - usedSpace;
+
+        Console.WriteLine($"{remainingSpace} Cubic meters left.");
+    }
+}
